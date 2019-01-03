eb2557e8bc3ffeb7ca251de1f34d8ac561642072
home <2455581815@qq.com>
2019-01-03 22:20:34 +0800

    提交

--- imooc-video-dev/app.js
+++ imooc-video-dev/app.js
@@ -1,6 +1,7 @@
 //app.js
 App({
-  serverUrl: "http://192.168.16.220:8081",
+  // serverUrl: "http://192.168.16.220:8081",
+  serverUrl: "http://192.168.0.103:8081",
   userInfo: null,
 
   setGlobalUserInfo: function(user) {
--- imooc-videos-dev/imooc-videos-dev-mini-api/src/main/java/com/imooc/controller/RegistLoginController.java
+++ imooc-videos-dev/imooc-videos-dev-mini-api/src/main/java/com/imooc/controller/RegistLoginController.java
@@ -1,15 +1,12 @@
 package com.imooc.controller;
 
 import com.imooc.pojo.Users;
-import com.imooc.pojo.vo.UsersVO;
 import com.imooc.service.UserService;
 import com.imooc.utils.IMoocJSONResult;
 import com.imooc.utils.MD5Utils;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.mapper.util.StringUtil;
 
-import java.util.UUID;
-
 @RestController
 @Api(value = "用户登录测试接口", tags = "注册和登录的controller")
-public class RegistLoginController extends BasicController{
+public class RegistLoginController {
 
 	@Autowired
 	private UserService userService;
@@ -35,7 +30,6 @@ public class RegistLoginController extends BasicController{
 		}
 		// 判断用户是否存在
 		boolean usernameIsExist = userService.queryUsernameIsExist(users.getUsername());
-		// 保存用户 注册信息
 		if (!usernameIsExist){
 			users.setNickname(users.getUsername());
 			users.setPassword(MD5Utils.getMD5Str(users.getPassword()));
@@ -46,27 +40,11 @@ public class RegistLoginController extends BasicController{
 		}else{
 			return IMoocJSONResult.errorMsg("用户名已经存在！");
 		}
-		//将注册的用户信息密码置为空返回去
+		// 保存用户 注册信息
 		users.setPassword("");
-		//向redis保存唯一标识加强交互
-		UsersVO suerVO = setUserRedisSessionToken(users);
-		return IMoocJSONResult.ok(suerVO);
-	}
-	/**
-	 * 使用用户唯一id加固定字符串组成key，生成唯一标识为value 存在redis中实现交互
-	 * @param userModel
-	 * @return
-	 */
-	public UsersVO setUserRedisSessionToken(Users userModel) {
-		//生成随机标识
-		String uniqueToken = UUID.randomUUID().toString();
-		redis.set(USER_REDIS_SESSION + ":" + userModel.getId(), uniqueToken, 1000 * 60 * 30);
-		UsersVO userVO = new UsersVO();
-		BeanUtils.copyProperties(userModel, userVO);
-		userVO.setUserToken(uniqueToken);
-		return userVO;
-	}
 
+		return IMoocJSONResult.ok(users);
+	}
 	@ApiOperation(value = "用户登录",notes = "用户登录接口")
 	@PostMapping(value = "/login")
 	public IMoocJSONResult login(@RequestBody Users user) throws Exception{
@@ -80,21 +58,10 @@ public class RegistLoginController extends BasicController{
 		Users userResult = userService.queryUserForLogin(username,MD5Utils.getMD5Str(user.getPassword()));
 		//返回
 		if (userResult != null){
-			//返回信息中用户密码置为空
 			userResult.setPassword("");
-			UsersVO userVO = setUserRedisSessionToken(userResult);
-			return IMoocJSONResult.ok(userVO);
+			return IMoocJSONResult.ok(userResult);
 		}else{
 			return IMoocJSONResult.errorMsg("用户名或者密码不正确，请重试！");
 		}
 	}
-	@ApiOperation(value = "用户注销",notes = "用户注销接口")
-	@ApiImplicitParam(name="userId", value="用户id", required=true,
-			dataType="String", paramType="query")
-	@PostMapping(value = "/logout")
-	public IMoocJSONResult logout(String userId) throws Exception {
-		redis.del(USER_REDIS_SESSION + ":" + userId);
-		return IMoocJSONResult.ok();
-	}
-
 }
